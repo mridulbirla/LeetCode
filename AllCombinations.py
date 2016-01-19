2af8f8d9c8e42658a59e84de70fc0326786c06d1
mridulbirla <mridulbirla@gmail.com>
2016-01-19 03:34:06 -0500

    Find all combination of given word

---
+++ AllCombinations.py
@@ -0,0 +1,34 @@
+import copy
+def print_matrix(matrix,s):
+
+    string_list=list(s)
+    final_str=""
+    for i in range(0,len(matrix)):
+        if matrix[i]==1:
+            final_str=final_str+string_list[i]
+    print final_str
+
+def all_combination(matrix,k,n,s):
+    mat=copy.copy(matrix)
+    if k==n:
+        mat[n]==0
+        print_matrix(mat,s)
+        mat[n]=1
+        print_matrix(mat,s)
+    else:
+        mat[k]=0
+        all_combination(mat,k+1,n,s)
+        mat[k]=1
+        all_combination(mat,k+1,n,s)
+
+
+def buildSubsequences(s):
+    ln=len(s)
+    sub_string=list(s)
+    matrix=[]
+    all_val=[]
+    for i in range(0,len(sub_string)):
+        matrix.append(0)
+    all_combination(matrix,0,ln-1,s)
+
+res = buildSubsequences("abc");
